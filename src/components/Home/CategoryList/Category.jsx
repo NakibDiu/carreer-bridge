6209a4183a86f0c28c887be5e2bc02b28aaf5e56
unknown <nakib15-3358@diu.edu.bd>
2023-04-12 04:44:32 +0600

    category section added

---
+++ src/components/Home/CategoryList/Category.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Category = ({ category }) => {
+  console.log(category);
+  return (
+    <div className="space-y-8 rounded-lg bg-[#f9f9ff] p-10">
+      <div className="w-[70px] h-[70px] bg-[#efecfe] p-4 rounded-lg">
+        <img src={category.icon} url="icon" />
+      </div>
+      <div className="space-y-2">
+        <h4 className="text-xl font-extrabold text-[#474747]">{category.title}</h4>
+        <p className="text-base font-medium">{category.numbers} Jobs Available</p>
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/components/Home/CategoryList/CategoryList.jsx
+++ src/components/Home/CategoryList/CategoryList.jsx
@@ -1,15 +1,23 @@
 import React from "react";
+import Category from "./Category";
 
 const CategoryList = ({ categories }) => {
   console.log(categories);
   return (
-    <div>
-      <div>
-        <h1>Job Category List</h1>
-        <p>
+    <div className="space-y-8 mt-14 lg:mt-24 xl:mt-32">
+      <div className="space-y-4">
+        <h1 className="text-5xl font-extrabold text-center">
+          Job Category List
+        </h1>
+        <p className="text-base text-center text-[#757575]">
           Explore thousands of job opportunities with all the information you
           need. Its your future
         </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {categories.map((category) => (
+          <Category category={category} key={category.id} />
+        ))}
       </div>
     </div>
   );
